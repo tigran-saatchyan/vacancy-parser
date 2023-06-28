6d7b510f4fe371229e8a4409cfa36bcda1788b14
Tigran Saatchyan TSA <mr.saatchyan@yandex.com>
2023-06-28 17:58:48 +0400

    [FEATURE] User interaction implemented

--- src/parser_hh.py
+++ src/parser_hh.py
@@ -1,25 +1,36 @@
 import requests
 
-from src.parser import Parser
+from src.parser import Parser, ParserMixin
 
 
-class HHParser(Parser):
+class HHParser(Parser, ParserMixin):
 
-    def parse_vacancies(self, keyword):
-        parameters = {
-            'page': 0,
-            'per_page': 10,
-            'text': keyword
-        }
-
-        headers = {
+    def __init__(self):
+        super().__init__()
+        self.keyword = ''
+        self.per_page = 20
+        self.url = 'https://api.hh.ru/vacancies'
+        self.headers = {
             'HHParser-User-Agent': 'Vacant/1.0 (mr.saatchyan@yandex.com)'
         }
+        self.parameters = {
+            'page': 1,
+            'per_page': self.per_page,
+            'text': self.keyword
+        }
 
-        response = requests.get(
-            'https://api.hh.ru/vacancies',
-            params=parameters,
-            headers=headers
-        )
+    def parse_vacancies(self, keyword, count):
+        result = []
+        pages = count // self.per_page + 1 \
+            if count % self.per_page else count // self.per_page
 
-        return response.json()
+        for page in range(1, pages + 1):
+
+            self.parameters.update({'page': page})
+
+            response = self.make_request(
+                self.url, self.parameters, self.headers
+            )
+            result.extend(response['items'])
+
+        return result
